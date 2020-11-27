dcee2e4c89013201aea5a6b7d77f04c4d9b3c509
Woloda <dorozovecvoloda@gmail.com>
2020-11-27 10:01:29 +0200

    Lab__8_1_8

---
+++ Lab_8.1(8)/Lab_8.1(8).cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+string InsertBefore(string& string, const char* para, const char* zamina);
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	string lit_r = "SQsdgdgQShhQQQSdhd";
+	cout << "Сфомований літерний рядок: " << endl << endl << lit_r << endl << endl;
+	char para[] = "SQ";
+	char para1[] = "QS";
+	char zamina[] = "***";
+
+	InsertBefore(lit_r, para, zamina);
+	InsertBefore(lit_r, para1, zamina);
+
+	cout << "Модифікований літерний рядок: " << endl << endl << lit_r << endl << endl;
+
+	return 0;
+}
+
+string InsertBefore(string& string, const char* para, const char* zamina)
+{
+	size_t pos = 0;
+	while ((pos = string.find(para)) != -1)
+	{
+		string.replace(pos, 2, zamina);
+		pos += 3;
+	}
+	return string;
+}
---
+++ UnitTest(Lab_8.1(8))/UnitTest(Lab_8.1(8)).cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_8.1(8)/Lab_8.1(8).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestLab818
+{
+	TEST_CLASS(UnitTestLab818)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			string lit_r = "SQsdgdgQShhQQQSdhd";
+			char para[] = "SQ";
+			char para1[] = "QS";
+			char zamina[] = "***";
+
+			InsertBefore(lit_r, para, zamina);
+			InsertBefore(lit_r, para1, zamina);
+
+			Assert::AreEqual(lit_r[0], '*');
+		}
+	};
+}
